ffd64726d5b727e6ea6086ff979b219590a77528
Stephanie Mrima <mrimasteph12@gmail.com>
2025-12-19 20:16:04 +0300

    Add Student Fee, Payment migrations

---
+++ database/migrations/2025_12_19_152223_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('academic_year_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('term_id')->constrained()->cascadeOnDelete();
+
+        $table->decimal('amount_paid', 10, 2);
+        $table->date('payment_date');
+
+        $table->string('payment_method'); // cash, mpesa, bank
+        $table->string('reference')->nullable();
+
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
