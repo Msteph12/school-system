530fce90be41374daf7fb520285dcb7cd437725c
Stephanie Mrima <mrimasteph12@gmail.com>
2025-12-21 13:37:44 +0300

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AcademicYearController;
 use App\Http\Controllers\Api\TermController;
+use App\Http\Controllers\Api\GradeController;
+use App\Http\Controllers\Api\SchoolClassController;
+use App\Http\Controllers\Api\SubjectController;
+use App\Http\Controllers\Api\GradeSubjectController;
+use App\Http\Controllers\Api\ClassStudentController;
+use App\Http\Controllers\Api\SubjectTeacherController;
+
 
 Route::apiResource('academic-years', AcademicYearController::class);
 Route::apiResource('terms', TermController::class);
+Route::apiResource('grades', GradeController::class);
+Route::apiResource('school-classes', SchoolClassController::class);
+Route::apiResource('subjects', SubjectController::class);
+Route::apiResource('grade-subjects', GradeSubjectController::class)->only([
+    'index', 'store', 'destroy']);
+Route::apiResource('class-students', ClassStudentController::class)->only([
+    'index', 'store', 'destroy']);
+Route::apiResource('subject-teachers', SubjectTeacherController::class)->only([
+    'index', 'store', 'destroy']);
