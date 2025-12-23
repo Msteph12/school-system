66f1b6924de6f35c43f2ca1655139730f4b8f098
Stephanie Mrima <mrimasteph12@gmail.com>
2025-12-23 16:45:35 +0300

    Add Roles table

--- database/migrations/2025_12_23_123500_create_roles_table.php
+++ database/migrations/2025_12_23_123500_create_roles_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
